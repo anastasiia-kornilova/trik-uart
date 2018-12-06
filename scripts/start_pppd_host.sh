b44e4bc1a0bcc76f4d7578c199dfab458b166537
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2018-12-06 16:14:40 +0300

    Support for both Linux and Windows machines

--- scripts/start_pppd_host.sh
+++ scripts/start_pppd_host.sh
@@ -3,4 +3,4 @@
 device="/dev/ttyUSB0"
 
 sudo stty -F $device raw
-sudo pppd $device 115200 10.0.5.1:10.0.5.2 proxyarp local noauth debug nodetach dump nocrtscts passive persist maxfail 0 holdoff 1
+sudo pppd $device 115200 10.0.5.1:10.0.5.2 connect 'chat -v -f winserver.chat'  proxyarp local noauth debug nodetach dump nocrtscts passive persist maxfail 0 holdoff 1
